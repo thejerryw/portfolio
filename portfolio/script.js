c35eb795610246ec79a5289d303149ea02adc107
Jerry Wang <68243688+thejerryw@users.noreply.github.com>
2025-04-08 10:36:24 -0700

    deleting portfolio directory to change layout

--- portfolio/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links"); // taking from html
-    const icon = document.querySelector(".hamburger-icon"); 
-    menu.classList.toggle("open"); // toggle changes hamburger menu into cross (open form)
-    icon.classList.toggle("open"); // toggle opens list
-}
